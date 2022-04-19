d4541222fe97cbca01ec4ef54701cedb8b490159
MintIsToothpaste <100202703+MintIsToothpaste@users.noreply.github.com>
2022-04-19 13:47:12 +0900

    #svelte pageArray button

---
+++ src/App.svelte
@@ -0,0 +1,61 @@
+<script>
+	import {db} from './main.js'
+	import "firebase/firestore"; 
+
+	function save() {
+    	var pageArray = {
+        ID : "Object0",
+        X : 218,
+        Y : 119,
+        W : 198,
+        H : 270,
+        Color : "C4C4C4",
+        Strok : "FFFFFF"
+        };
+
+        var pageArray_str = JSON.stringify(pageArray);
+
+        db.collection('save').add(pageArray).then((result)=>{
+            window.location.href = '/index.html'
+        }).catch((error)=>{
+            console.log(err)
+        })
+  	}
+
+	function recall() {
+    	db.collection('save').get().then((결과)=>{
+            결과.forEach((doc)=>{
+                console.log(doc.data())
+                
+                var pageArray_str = JSON.stringify(doc.data());
+
+                var 템플릿 = `<div class="design">
+                <div class="flex-grow-1 p-4">
+                    <h5 class="price">${pageArray_str}</h5>
+                </div>
+                </div>`;
+                $('.container').append(템플릿) 
+            })
+        })
+  	}
+
+</script>
+
+<style>
+	button {
+    color:white;
+    background:  #01BED6;
+    border-radius: 0.3rem;
+    width: 6rem;
+    height: 2.2rem;
+    padding: 0.5rem 1rem;
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    outline: none;
+    border: 0px;
+  }
+</style>
+
+<button on:click={save}>저장하기</button>
+<button on:click={recall}>불러오기</button>
---
+++ src/main.js
@@ -0,0 +1,20 @@
+import App from './App.svelte';
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+var app = new App({
+	target: document.body
+});
+
+const firebaseConfig = {
+	apiKey: "AIzaSyB5CjIBc8dsRFh3Uyc5qDegxPFD3jh6X1Q",
+	authDomain: "pagearraysave.firebaseapp.com",
+	projectId: "pagearraysave",
+	storageBucket: "pagearraysave.appspot.com",
+	messagingSenderId: "874287836949",
+	appId: "1:874287836949:web:1e4b475aab8b99f987831c"
+};
+
+firebase.initializeApp(firebaseConfig);
+export const db = firebase.firestore();
+export default app;
